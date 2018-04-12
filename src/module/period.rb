c526aeae258c097e1e04a029da9c7c7155aa8a63
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2018-04-13 02:43:48 +0900

    Modify period module according to period time change

--- src/module/period.rb
+++ src/module/period.rb
@@ -3,42 +3,42 @@ module Period
   def period1(day)
     [
       DateTime.new(day.year, day.month, day.day, 9, 0, 0, 0.375),
-      DateTime.new(day.year, day.month, day.day, 10, 30, 0, 0.375)
+      DateTime.new(day.year, day.month, day.day, 10, 40, 0, 0.375)
     ].freeze
   end
 
   def period2(date)
     [
-      DateTime.new(date.year, date.month, date.day, 10, 40, 0, 0.375),
-      DateTime.new(date.year, date.month, date.day, 12, 10, 0, 0.375)
+      DateTime.new(date.year, date.month, date.day, 10, 50, 0, 0.375),
+      DateTime.new(date.year, date.month, date.day, 12, 30, 0, 0.375)
     ].freeze
   end
 
   def period25(date)
     [
-      DateTime.new(date.year, date.month, date.day, 12, 10, 0, 0.375),
-      DateTime.new(date.year, date.month, date.day, 13, 10, 0, 0.375)
+      DateTime.new(date.year, date.month, date.day, 12, 30, 0, 0.375),
+      DateTime.new(date.year, date.month, date.day, 13, 20, 0, 0.375)
     ].freeze
   end
 
   def period3(day)
     [
-      DateTime.new(day.year, day.month, day.day, 13, 10, 0, 0.375),
-      DateTime.new(day.year, day.month, day.day, 14, 40, 0, 0.375)
+      DateTime.new(day.year, day.month, day.day, 13, 20, 0, 0.375),
+      DateTime.new(day.year, day.month, day.day, 15, 0, 0, 0.375)
     ].freeze
   end
 
   def period4(day)
     [
-      DateTime.new(day.year, day.month, day.day, 14, 50, 0, 0.375),
-      DateTime.new(day.year, day.month, day.day, 16, 20, 0, 0.375)
+      DateTime.new(day.year, day.month, day.day, 15, 10, 0, 0.375),
+      DateTime.new(day.year, day.month, day.day, 16, 50, 0, 0.375)
     ].freeze
   end
 
   def period5(day)
     [
-      DateTime.new(day.year, day.month, day.day, 16, 30, 0, 0.375),
-      DateTime.new(day.year, day.month, day.day, 18, 0, 0, 0.375)
+      DateTime.new(day.year, day.month, day.day, 17, 0, 0, 0.375),
+      DateTime.new(day.year, day.month, day.day, 17, 50, 0, 0.375)
     ].freeze
   end
 
